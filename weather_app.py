d6122fb02a5d61441fcdf35339234aa138ecb670
Paul D <154512492+Mayne911@users.noreply.github.com>
2024-05-18 19:57:18 +0200

    First ideas of my little weather app projetct - starting soon

---
+++ weather_app.py
@@ -0,0 +1,27 @@
+
+#Idea : Make a little app that tells you the weather in a city you input. You can also get the weather in your current location & the weather in a random city. 
+#You can save until 5 cities and see the weather in them and delete them if you want. You can target some hours in the future to see the weather in that hour. For exemple you can see the weather in Paris at 5pm to know what
+#to wear if you go out at that time.
+
+#I want to do this project because it is a good way to practice the use of APIs and the use of a database to store data, I think . And I think it is a good idea to have a little app that tells you the weather in a city you want to know.
+#at the time you want to know. 
+
+#I will use the OpenWeatherMap API to get the weather data and the sqlite3 library to store the data in a database. 
+#I will use the requests library to make the requests to the API. I will use the tkinter library to make the GUI of the app.
+
+#I will start by making the GUI of the app. I will make a window with a label that tells you to enter a city name and an entry where you can enter the city name.
+#I will make a button that when you click it, it will show you the weather in the city you entered. I will make a label that shows you the weather data.
+#I will make a button that when you click it, it will show you the weather in your current location. I will make a label that shows you the weather data.
+#I will make a button that when you click it, it will show you the weather in a city you entered at a specific hour. I will make a label that shows you the weather data.
+
+#TO DO: Make the GUI of the app.
+#TO DO: Make the requests to the API to get the weather data.
+#TO DO: Make the database to store the weather data.
+#TO DO: Make the functions to save the weather data in the database.
+#TO DO: Make the functions to delete the weather data in the database.
+#TO DO: Make the functions to show the weather data in the database.
+#TO DO: Make the functions to show the weather data in the GUI.
+#TO DO: Make the functions to show the weather data in the GUI at a specific hour.
+#TO DO: Make the functions to show the weather data in the GUI at your current location.
+
+#WORK IN PROGRESS !
